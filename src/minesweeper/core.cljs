4365bbf03da31cd199b3019930672ffbfda24f83
Jukka Aittola <jaittola@iki.fi>
2015-12-27 18:22:09 +0200

    Add a drop-down for restarting the game

--- src/minesweeper/core.cljs
+++ src/minesweeper/core.cljs
@@ -11,21 +11,30 @@
 (defonce app-state (r/atom (m/make-empty-minefield 5 5)))
 (defonce game-over (atom false))
 
+(defn finish-game []
+  (swap! game-over #(identity true))
+  (swap! app-state #(assoc % :game-over true)))
+
+(defn update-game-state [minefield]
+  (swap! game-over (fn [_ mf] (:game-over mf)) minefield)
+  (swap! app-state (fn [_ mf] mf) minefield))
+
+
 (defn slot-clicked [row col]
   (println "Slot clicked at" row col)
   ;; This is a hack to avoid reagent's warnings about lazy
   ;; sequences. I'm not sure about the reason but this trickery
   ;; seems to work around it.
   (when (not @game-over)
-    (let [minefield (m/slot-clicked @app-state row col)]
-      (swap! game-over (fn [_ mf] (:game-over mf)) minefield)
-      (swap! app-state (fn [_ mf] mf) minefield))
+    (update-game-state (m/slot-clicked @app-state row col))
     (when (= 0 (m/unchecked-slots-without-mines @app-state))
       (finish-game))))
 
-(defn finish-game []
-  (swap! game-over #(identity true))
-  (swap! app-state #(assoc % :game-over true)))
+(defn restart-clicked [size]
+  (println "Restart-clicked with size" size)
+  (let [n (js/Number size)]
+   (when-not (js/isNaN n)
+     (update-game-state (m/make-empty-minefield n n)))))
 
 (defn render-unchecked-slot [slot]
   ^{:key (:id slot) } [:td [:a {:class "slot unchecked"
@@ -57,11 +66,22 @@
   (let [rnum (:row (first row))]
     ^{:key (str "r_" rnum)} [:tr (map render-slot row)]))
 
+(defn render-restart-dropdown []
+  [:select {:class "restart-dropdown"
+            :on-change #(restart-clicked (-> % .-target .-value))
+            :value "r"}
+    [:option {:value "r"} "Restart game"]
+    [:option {:value "5"} "Small: 5x5"]
+    [:option {:value "10"} "Medium: 10x10"]
+    [:option {:value "20"} "Large: 20x20"]])
+
 (defn render-minefield []
   (let [field (:field @app-state)
         row-vector (partition-by #(:row %) field)]
-    [:div
-     [:table [:tbody (map render-minefield-row row-vector)]]]))
+    [:div {:class "content-container"}
+     [:div
+      [:table [:tbody (map render-minefield-row row-vector)]]]
+     (render-restart-dropdown)]))
 
 (defn do-render []
   (r/render [render-minefield]
