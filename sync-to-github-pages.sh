3b7e083e5550de035cb888c6f4b2192396445e2d
Jukka Aittola <jaittola@iki.fi>
2015-12-27 22:42:52 +0200

    Add script for building and syncing to github pages

---
+++ sync-to-github-pages.sh
@@ -0,0 +1,32 @@
+#!/bin/bash -e
+
+me=`whoami`
+url="${1:-git@github.com:${me}/${me}.github.io.git}"
+wd=`pwd`
+
+# Create a temporary dir and set up cleaning at exit.
+checkoutdir=`mktemp -d "${wd}/sync-to-github.XXXXXXXX"`
+trap "rm -rf \"$checkoutdir\"" EXIT
+
+# Helper variables
+pagesdir="${checkoutdir}/pages"
+msdir="${pagesdir}/minesweeper"
+
+# Fetch sources.
+git clone "$url" "$pagesdir"
+
+# Build
+lein clean && lein cljsbuild once min
+
+# Copy code to checked out source dir
+cd resources/public
+rsync -av . "$msdir"
+
+# Commit
+cd ${msdir}
+git add .
+git commit -m "Update minesweeper"
+git push origin master
+
+cd "$wd"
+lein clean
